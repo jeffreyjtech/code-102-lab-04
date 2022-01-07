79e6679d8a1d3c80850815d70f0020fe77b23ee6
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-01-07 15:41:28 -0800

    Changed a prompt to be more like a password check, cleaned up HTML and JS

--- scripts.js
+++ scripts.js
@@ -18,28 +18,30 @@ let datestring = getDateString()
 // Writes the datestring into the "date-display" element
 document.getElementById('date-display').textContent += datestring
 
-// Asks the user if they want to "delete" the webpage, won't accept a yes though
-function askUserDelete(){
-    let answer = prompt('Delete this webpage before viewing?')
+// Asks the user if they really want to see the webpage, will only accept "yes"
+function askUserToSee(){
+    let answer = prompt('This webpage is not great. Are you sure you want to see it?')
     console.log(answer)
-    if (answer.toLowerCase() === 'yes'){
+    if (answer.toLowerCase() !== 'yes'){
         do{
-        answer = prompt('Actually you can\'t, try a different answer')
-        } while (answer.toLowerCase() === 'yes')
+        answer = prompt('Well, it\'d be rude to not see it. You should just say yes.')
+        } while (answer.toLowerCase() !== 'yes')
     }
 }
 
+// Changes the body's text color based on the user's input
 function colorChoice(){
     let usercolor = 'black'
     usercolor = prompt('What color would you like the heading text?')
     document.body.style.color = usercolor;
 }
 
+// asks how many mountains user wants to see
 function howManyMountains(){
-
-    // asks how many mountains user wants to see, minus 1 in order to adjust for the image array index starting at 0, not 1.
+    // The user input needs to be subtracted by 1 to adjust for the image array index starting at 0
     let mtqty = prompt('How many mountain pictures do you want to see? Choose from 1 to 5.') - 1
     do{
+        // calls the addImageElement function, passing in the URL it has pulled from the array and the current mtqty value
         addImageElement(mtURLs[mtqty], mtqty)
         mtqty--
     } while ((mtqty >= 0) && (mtqty < 4))
@@ -49,18 +51,18 @@ function howManyMountains(){
 // sets its src to the URL passed in from the mtURL array
 // then uses the index passed in to choose an insert location aka insert node
 // which happen to be <p> elements already in the HTML with integer ids from 1 to 5
-// and finally the insertBefore() function needs "parent node" to look for, aka the parent element of the <p> elements
+// and finally the insertBefore() function is used to add in the element we've just constructed.
+// The insertBefore() function must be called on the "parent node" of the elements insertBefore() is looking for.
 function addImageElement(URL, qty){
     let newImg = document.createElement('img')
     newImg.src = URL
-    let insertlocation = document.getElementById(qty.toString) // since ids are strings, it makes me feel better to turn the qty into a string
+    let insertlocation = document.getElementById(qty.toString) // the qty parameter is used here so that different <p> elements recieve a given <img>, this avoids any overwriting.
     document.getElementById('galleryparent').insertBefore(newImg, insertlocation)
 }
 
-// call prompt functions
-askUserDelete()
+// calls all the various functions above
+askUserToSee()
 colorChoice()
 howManyMountains()
 
-// pageDeleter is called last so it overwites content changed by the previous code
 console.log('My javascript ran to the end')
